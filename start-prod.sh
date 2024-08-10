f8d6e58f0153759af0da2195326b397e5edbff92
dongyeon <uuas5866@naver.com>
2024-08-10 20:00:37 +0900

    Test: 왜 안되누

--- start-prod.sh
+++ start-prod.sh
@@ -1,7 +1,5 @@
 docker-compose -f docker-compose-prod.yml pull
 
-docker-compose down --rmi all --volumes --remove-orphans
-
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose-prod.yml up --build -d
 
 docker rmi -f $(docker images -f "dangling=true" -q) || true
