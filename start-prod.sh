14a2bbcbadf7506ba60861daa32f381720d4f30a
dongyeon <uuas5866@naver.com>
2024-08-10 19:25:09 +0900

    Test: 백 서버 잘되나 확인

--- start-prod.sh
+++ start-prod.sh
@@ -1,5 +1,7 @@
 docker-compose -f docker-compose-prod.yml pull
 
+docker-compose down --rmi all --volumes --remove-orphans
+
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose-prod.yml up --build -d
 
 docker rmi -f $(docker images -f "dangling=true" -q) || true
