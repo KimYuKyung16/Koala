3eba543cac0047517be70758916aca834daa6f5c
dongyeon <uuas5866@naver.com>
2024-08-10 18:59:23 +0900

    Test: jenkins 테스트 중..

--- backend/koala_back/src/main/java/com/ssafy/domain/sentence/controller/SentenceController.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/sentence/controller/SentenceController.java
@@ -38,7 +38,7 @@ public class SentenceController {
 		if (sentenceList == null || sentenceList.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 		} else {
-			return ResponseEntity.status(HttpStatus.OK).body("젠킨스 개가튼거888");
+			return ResponseEntity.status(HttpStatus.OK).body("젠킨스 개가튼거999");
 		}
 	}
 
