96c46d19a52249a4bd2e364ebaa59b31428a530c
dongyeon <uuas5866@naver.com>
2024-08-12 12:42:48 +0900

    Test: 젠킨스 테스틋

--- backend/koala_back/src/main/java/com/ssafy/domain/sentence/controller/SentenceController.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/sentence/controller/SentenceController.java
@@ -38,7 +38,7 @@ public class SentenceController {
 		if (sentenceList == null || sentenceList.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 		} else {
-			return ResponseEntity.status(HttpStatus.OK).body(sentenceList+"뭐가 문제일까3");
+			return ResponseEntity.status(HttpStatus.OK).body(sentenceList+"뭐가 문제일까4");
 		}
 	}
 
